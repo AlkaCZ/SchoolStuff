c5576649196095ddb1499f8f270009e15f96a9f6
Petr <petrjelinek35@seznam.cz>
2020-12-11 14:12:01 +0100

    Přidejte soubory projektu.

---
+++ ElloHttp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net.Http;// Adding usings
+using Newtonsoft.Json;//
+using System.Text;//
+using System.Threading.Tasks;//
+
+namespace ElloHttp
+{
+    class Person
+    {
+        [JsonProperty("jmeno")] //Otagovat vlastnosti
+        public string Jmeno { get; set; }
+
+        [JsonProperty("prijmeni")]
+        public string Prijmeni { get; set; }
+
+        public override string ToString() => $"Jsem {Jmeno} {Prijmeni}";
+
+    }
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Person p = new Person { Jmeno = "Olex", Prijmeni = "Sek" };
+
+            var pJson = JsonConvert.SerializeObject(p);
+
+            HttpClient htc = new HttpClient(); //Novy klient
+
+            HttpContent content = new StringContent(pJson, Encoding.UTF8, "application/json");//Novy kontent zpravy
+
+            try
+            {
+                HttpResponseMessage responseMessage = await htc.PostAsync("https://ptsv2.com/t/iwxjh-1607691193/post", content);//Post na server
+                Console.WriteLine(responseMessage);//Hlaska serveru
+
+            }
+            catch (HttpRequestException)
+            {
+
+               Console.WriteLine("error");// Napr. server nekomunikuje apod.
+            }
+
+
+        }
+    }
+}
---
+++ ElloJson/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Newtonsoft.Json; //Must add using
+
+namespace ElloJson
+{
+    class Person
+    {
+        [JsonProperty("jmeno")] //Otagovat vlastnosti
+       public  string Jmeno { get; set; }
+
+        [JsonProperty("prijmeni")]
+        public string Prijmeni { get; set; }
+
+        public override string ToString() => $"Jsem {Jmeno} {Prijmeni}";
+
+    }
+    class Group //Class in class
+    {
+        [JsonProperty("mama")]
+        public Person p1 { get; set; }
+
+        [JsonProperty("tata")]
+        public Person p2 { get; set; }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person olex = new Person { Jmeno = "Olex", Prijmeni = "Sek" };
+            Person mira = new Person { Jmeno = "Mira", Prijmeni = "Palek" };
+
+            Group g = new Group { p1 = olex, p2 = mira };
+
+            //Konvertování do Jsonu aka. Serializace
+
+            var JsonPerson = JsonConvert.SerializeObject(olex);
+            var JsonGroup = JsonConvert.SerializeObject(g);
+
+            Console.WriteLine(JsonPerson);
+            Console.WriteLine(JsonGroup);
+            //Konvertování z Jsonu do Person objektu aka. Deserizialzace
+            Person test = JsonConvert.DeserializeObject<Person>(JsonPerson);
+
+            Console.WriteLine(test);
+            Console.ReadLine();
+
+        }
+    }
+}
